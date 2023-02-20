23339396d78aaa79a080aadc087cd4b527758082
Mihir Patel <mihir.v.patel7@gmail.com>
2023-02-19 17:28:03 -0800

    param lr

--- benchmark.py
+++ benchmark.py
@@ -63,6 +63,7 @@ parser.add_argument('--wandb_project', type=str)
 
 # Trainer arguments
 parser.add_argument('--device_train_microbatch_size', type=int)
+parser.add_argument('--learning_rate', type=float, default=1.0e-4)
 args = parser.parse_args()
 
 class StableDiffusion(composer.models.ComposerModel):
@@ -148,7 +149,7 @@ def main(args):
         if not args.disable_vae_clip:
             model.vae.enable_xformers_memory_efficient_attention()
 
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=1.0e-4, weight_decay=0.001)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=args.learning_rate, weight_decay=0.001)
     lr_scheduler = composer.optim.ConstantScheduler()
 
     device_batch_size = args.batch_size // dist.get_world_size()
