32b5006d1d309753fe30c2290497ca11639aef72
Mihir Patel <mihir.v.patel7@gmail.com>
2023-02-21 11:07:10 -0800

    foreachTrue

--- benchmark.py
+++ benchmark.py
@@ -164,7 +164,7 @@ def main(args):
         if not args.disable_vae_clip:
             model.vae.enable_xformers_memory_efficient_attention()
 
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=args.learning_rate, weight_decay=0.001)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=args.learning_rate, weight_decay=0.001, foreach=True)
     lr_scheduler = composer.optim.ConstantScheduler()
 
     device_batch_size = args.batch_size // dist.get_world_size()
